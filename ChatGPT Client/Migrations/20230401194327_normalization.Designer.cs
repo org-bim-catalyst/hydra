eb07e4a5215540c80d17bdfc3350ab9e4eb49048
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2023-04-01 23:59:07 +0400

    Update connection strings.

--- ChatGPT Client/Migrations/20230401194327_normalization.Designer.cs
+++ ChatGPT Client/Migrations/20230401194327_normalization.Designer.cs
@@ -4,6 +4,7 @@ using AskLucy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AskLucy.Migrations
 {
     [DbContext(typeof(ChatGPT_ClientContext))]
-    partial class ChatGPT_ClientContextModelSnapshot : ModelSnapshot
+    [Migration("20230401194327_normalization")]
+    partial class normalization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
