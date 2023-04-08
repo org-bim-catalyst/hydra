24f3ce026de37177d394cf5cc15624944a4ccc52
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2023-04-09 00:55:32 +0400

    Adding DoB, FN, LN, PP

---
+++ ChatGPT Client/Areas/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AskLucy.Areas.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public byte[]? ProfilePicture { get; set; }
+    }
+}
