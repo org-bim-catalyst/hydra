5fe94b3c9f9c8544731fae51c0c8e7ac9dc66578
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2023-04-29 22:12:28 +0400

    Adjust the error message class.

--- ChatGPT Client/Scripts/ts/core/error-manager.js
+++ ChatGPT Client/Scripts/ts/core/error-manager.js
@@ -11,17 +11,8 @@ var ErrorManager = /** @class */ (function () {
         else {
             message = errorThrown;
         }
-        var modal = "<div class=\"modal fade\" id=\"modal-error-message\" tabindex=\"-1\" aria-labelledby=\"modal-label-error-message\" aria-hidden=\"true\" data-target=\"#staticBackdrop\">\n                        <div class=\"modal-dialog modal-dialog-centered\">\n                            <div class=\"modal-content\">\n                                <div class=\"modal-header\">\n                                    <h5 class=\"modal-title\" id=\"modal-label-error-message\">OOOPS something wrong happened!!!</h5>\n                                    <button type=\"button\" class=\"btn-close\" data-mdb-dismiss=\"modal\" aria-label=\"Close\"></button>\n                                </div>\n                                <div class=\"modal-body\">\n                                    <div class=\"col-md-6 mx-auto\">\n                                        <p class=\"text-danger\" id=\"p-error-message\">\n                                                ".concat(message, "\n                                        </p>\n                                    </div>\n                                </div>\n\n                                <div class=\"modal-footer\">\n                                    <button type=\"button\" class=\"btn btn-sm btn-danger\" data-mdb-dismiss=\"modal\" id=\"button-translate-message\">Close</button>\n                                </div>\n                            </div>\n                        </div>\n                    </div>");
+        var modal = "<div class=\"modal fade\" id=\"modal-error-message\" tabindex=\"-1\" aria-labelledby=\"modal-label-error-message\" aria-hidden=\"true\" data-target=\"#staticBackdrop\">\n                        <div class=\"modal-dialog modal-dialog-centered\">\n                            <div class=\"modal-content\">\n                                <div class=\"modal-header\">\n                                    <h5 class=\"modal-title\" id=\"modal-label-error-message\">OOOPS something wrong happened!!!</h5>\n                                    <button type=\"button\" class=\"btn-close\" data-mdb-dismiss=\"modal\" aria-label=\"Close\"></button>\n                                </div>\n                                <div class=\"modal-body\">\n                                    <div class=\"col-md-12\">\n                                        <p class=\"text-danger\" id=\"p-error-message\">\n                                                ".concat(message, "\n                                        </p>\n                                    </div>\n                                </div>\n\n                                <div class=\"modal-footer\">\n                                    <button type=\"button\" class=\"btn btn-sm btn-danger\" data-mdb-dismiss=\"modal\" id=\"button-translate-message\">Close</button>\n                                </div>\n                            </div>\n                        </div>\n                    </div>");
         $(modal).modal('toggle');
-    };
-    ErrorManager.prototype.isJsonObject = function (strData) {
-        try {
-            JSON.parse(strData);
-        }
-        catch (e) {
-            return false;
-        }
-        return true;
     };
     return ErrorManager;
 }());
