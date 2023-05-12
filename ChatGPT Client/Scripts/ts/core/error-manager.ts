8d5618d6342d359ec16d033dc6a0328d696632b9
Mustafa Salaheldin <mustafa.salaheldin@yahoo.com>
2023-05-12 18:06:52 +0400

    Fixing the Katex issue, still need more fix.

--- ChatGPT Client/Scripts/ts/core/error-manager.ts
+++ ChatGPT Client/Scripts/ts/core/error-manager.ts
@@ -1,5 +1,34 @@
 ﻿export default class ErrorManager {
 
+    logExceptionError(errorThrown) {
+
+        let message = errorThrown;
+
+        let modal = `<div class="modal fade" id="modal-error-message" tabindex="-1" aria-labelledby="modal-label-error-message" aria-hidden="true" data-target="#staticBackdrop">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modal-label-error-message">OOOPS something wrong happened!!!</h5>
+                                    <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="col-md-12">
+                                        <p class="text-danger" id="p-error-message">
+                                                ${message}
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-sm btn-danger" data-mdb-dismiss="modal" id="button-translate-message">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+
+        $(modal).modal('toggle');
+    }
+
     logAjaxError(XMLHttpRequest, textStatus, errorThrown) {
 
         let message = '';
