9450c8a7a4fbd564b70c6eb515bfc35175832137
Mustafa Salaheldin <mustafa.salaheldin@hotmail.com>
2026-07-29 11:49:27 +0300

    fix: merge the frontend and webAPI into one application.

--- Ask Lucy/Scripts/ts/interfaces/IUserChat.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=IUserChat.js.map
--- Ask Lucy/Services/AuthMessageSenderOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AskLucy.Services
-{
-    public class AuthMessageSenderOptions
-    {
-        public string? SendGridKey { get; set; }
-    }
-}
